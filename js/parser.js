fcdc23dd0f6e153e8b217b6f984c534e8345e6c2
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2022-03-22 13:37:14 +0530

    [+] Add pagination module to parser

--- js/parser.js
+++ js/parser.js
@@ -5,15 +5,17 @@ function strip(html) {
 }
 
 function home_data_parser(raw_data) {
+    const total_pages = parseInt(raw_data['totalCount'] / 20);
     const ranker_list = raw_data['rankerLists']
     var data_container = [];
     for (var i in ranker_list) {
         var name = strip(JSON.stringify(ranker_list[i]['name'])).slice(1, -1)
         var id = JSON.stringify(ranker_list[i]['id'])
-        var description = strip(JSON.stringify(ranker_list[i]['description'])).slice(0, 250) + "...";
+        var description = strip(JSON.stringify(ranker_list[i]['description'])).slice(1, 300) + "...";
         var img = JSON.stringify(ranker_list[i]['image']['url'])
         data_container.push.apply(data_container, [name, id, description, img]);
     }
+    data_container.push.apply(data_container, [total_pages])
     return data_container;
 }
 
