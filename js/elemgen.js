f31a60f8f171ca9336d90c7f95d1e7b2babafd3b
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2022-03-22 14:48:28 +0530

    [+] Add next/prev module

--- js/elemgen.js
+++ js/elemgen.js
@@ -12,8 +12,13 @@ function gen_card_code(img_url, title, description, id) {
 }
 
 function get_pagination_code(current_page, last_page) {
-    let pagination_code = "";
-
+    if (current_page < 1) {
+        current_page = 1;
+    }
+    if (current_page > last_page) {
+        current_page = last_page;
+    }
+    let pagination_code = '<a onclick="query4npage(' + (current_page - 1).toString() + ')">⭅</a>';
     // for the first three
     for (let i = 1; i <= 3; i++){
         pagination_code += '<a onclick="query4npage('+i.toString()+')"';
@@ -25,7 +30,7 @@ function get_pagination_code(current_page, last_page) {
 
     pagination_code += '<a>...</a>';
     
-    if (current_page > 4 && current_page < last_page - 4) {
+    if (current_page > 3 && current_page < last_page - 3) {
         for (let i = current_page-1; i <= current_page+1; i++) {
             pagination_code += '<a onclick="query4npage(' + i.toString() + ')"';
             if (current_page === i) {
@@ -44,6 +49,8 @@ function get_pagination_code(current_page, last_page) {
         }
         pagination_code += '>' + i.toString() + "</a>";
     }
+
+    pagination_code += '<a onclick="query4npage(' + (current_page + 1).toString() + ')">⭆</a>';
     return pagination_code;
 }
 
