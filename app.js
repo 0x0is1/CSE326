7b62d8caf83e2b38c24b4d16d1b91c9d176e0be4
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2022-03-22 13:38:23 +0530

    [+] Update app module

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-import * as requests from './js/requests.js';
-import * as parser from './js/parser.js'
-import * as codegen from './js/elemgen.js'
+import * as events_handler from './js/events_handler.js';
 
-const urls = new requests.SubUrls(null, 50);
+window.onload = () => {
+    events_handler.set_top_query4npage(1);
+    events_handler.set_pagination_bar(1, 96);
+}
 
-requests.fetch_url("GET", requests.BASE_URL + urls.browse_query).then(resp => {
-    const parsed_data = parser.home_data_parser(JSON.parse(resp));
-    var elem = document.getElementsByClassName("container")[0];
-    var card;
-    for (var i = 1; i < parsed_data.length; i += 4) {
-        card = codegen.gen_card_code(parsed_data[i + 2], parsed_data[i - 1], parsed_data[i + 1])
-        elem.innerHTML += card;
-    }
-})
+window.query4npage = (n) => {
+    events_handler.set_top_query4npage(n)
+    events_handler.set_pagination_bar(n, 96);
+}
+
+
