48d2005e58746633f306f95ce0a32bb5a87d879c
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2022-03-23 13:41:42 +0530

    [+] Add code generator module for navbar and publisher

--- js/elemgen.js
+++ js/elemgen.js
@@ -1,3 +1,19 @@
+function gen_navbar_code(active_class) {
+    const menu_items = ['Home', 'Publish', 'Vote', 'Other Projects', 'About'];
+    const hrefs = ['/', '/pages/publish.html', '/pages/vote.html', '/pages/projects.html', '/pages/about.html'];
+    var code = 
+        '<a class="logo">Top 100 of everything</a><div class="menu-toggle"></div><nav><ul>';
+    for (let i = 0; i < menu_items.length; i++){
+        code += '<li><a href="' + hrefs[i] + '"';
+        if (active_class === i) {
+            code += ' class="active" ';
+        }
+        code += '>' + menu_items[i] + '</a></li>';
+    }
+    code += '</ul></nav><div class="clearfix"></div>';
+    return code;
+}
+
 function gen_card_code(img_url, title, description, id) {
     var card_code =
         '<a class="card" onclick="gen_rank_page(' + id + ')">\
@@ -20,7 +36,7 @@ function get_pagination_code(current_page, last_page) {
     }
     let pagination_code = '<a onclick="query4npage(' + (current_page - 1).toString() + ')">⭅</a>';
     // for the first three
-    for (let i = 1; i <= 3; i++){
+    for (let i = 1; i <= 2; i++){
         pagination_code += '<a onclick="query4npage('+i.toString()+')"';
         if (current_page === i) {
             pagination_code += " class='activepg'";
@@ -30,7 +46,7 @@ function get_pagination_code(current_page, last_page) {
 
     pagination_code += '<a>...</a>';
     
-    if (current_page > 3 && current_page < last_page - 3) {
+    if (current_page > 2 && current_page < last_page - 2) {
         for (let i = current_page-1; i <= current_page+1; i++) {
             pagination_code += '<a onclick="query4npage(' + i.toString() + ')"';
             if (current_page === i) {
@@ -42,7 +58,7 @@ function get_pagination_code(current_page, last_page) {
     }
 
     // for the last three
-    for (let i = last_page-2; i <= last_page; i++) {
+    for (let i = last_page-1; i <= last_page; i++) {
         pagination_code += '<a onclick="query4npage(' + i.toString() + ')"';
         if (current_page === i) {
             pagination_code += " class='activepg'";
@@ -54,4 +70,4 @@ function get_pagination_code(current_page, last_page) {
     return pagination_code;
 }
 
-export { gen_card_code, get_pagination_code };
+export { gen_card_code, get_pagination_code, gen_navbar_code };
