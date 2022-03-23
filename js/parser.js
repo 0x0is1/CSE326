95b4b4dc0af210f8fb8d67805d20c05565588db0
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2022-03-23 13:40:16 +0530

    [+] Add parser module for publishers page

--- js/parser.js
+++ js/parser.js
@@ -5,18 +5,35 @@ function strip(html) {
 }
 
 function home_data_parser(raw_data) {
-    const total_pages = parseInt(raw_data['totalCount'] / 20);
-    const ranker_list = raw_data['rankerLists']
+    const total_pages = parseInt(raw_data.totalCount / 20);
+    const ranker_list = raw_data.rankerLists;
     var data_container = [];
     for (var i in ranker_list) {
-        var name = strip(JSON.stringify(ranker_list[i]['name'])).slice(1, -1)
-        var id = JSON.stringify(ranker_list[i]['id'])
-        var description = strip(JSON.stringify(ranker_list[i]['description'])).slice(1, 300) + "...";
-        var img = JSON.stringify(ranker_list[i]['image']['url'])
+        var name = strip(JSON.stringify(ranker_list[i].name)).slice(1, -1);
+        var id = JSON.stringify(ranker_list[i].id);
+        var description = strip(JSON.stringify(ranker_list[i].description)).slice(1, 300) + "...";
+        var img = JSON.stringify(ranker_list[i].image.url);
         data_container.push.apply(data_container, [name, id, description, img]);
     }
-    data_container.push.apply(data_container, [total_pages])
+    data_container.push.apply(data_container, [total_pages]);
     return data_container;
 }
 
-export { home_data_parser, strip };
+function ranked_lists_parser(raw_data) {
+    const ranked_list = raw_data.rankerLists;
+    var data_container = [];
+    for (let i in ranked_list) {
+        const id = JSON.stringify(ranked_list[i].id);
+        const name = strip(JSON.stringify(ranked_list[i].name)).slice(1, -1);
+        const user_name = strip(JSON.stringify(ranked_list[i].user.userName)).slice(1, -1);
+        const pubdt = JSON.stringify(ranked_list[i].publishDate);
+        const moddt = JSON.stringify(ranked_list[i].modifiedOn);
+        const stats = ranked_list[i].listStats;
+        const votes = JSON.stringify(stats.totalVotes);
+        const views = JSON.stringify(stats.viewCount);
+        const image = JSON.stringify(ranked_list[i].image.url);
+        data_container.push.apply(data_container, [id, name, user_name, image, pubdt, moddt, votes, views]);
+    }
+    return data_container;
+}
+export { home_data_parser, strip, ranked_lists_parser };
