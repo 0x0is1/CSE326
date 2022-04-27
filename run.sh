ecabdcec1b980fd7587c5e2a521d2b1ed774182b
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2022-04-28 01:57:03 +0530

    Minor change

--- run.sh
+++ run.sh
@@ -1,13 +1,2 @@
-function check_installed(){
-echo "[*] Checking if http-server is installed..."
-package="http-server"
-if [ `npm list -g | grep -c $package` -eq 0 ]; then
-    echo "[-] Package not installed. Installing..."
-    sudo npm install $package -g
-else
-    echo "[+] Package already installed."
-fi
-}
-
-check_installed
+npm install http-server
 http-server -c1
